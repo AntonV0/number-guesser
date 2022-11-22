a3f8efc2356ba542037ffabd384878d6de6e92c1
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-11-22 14:23:00 +0000

    Final commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Number Guesser
+## Project #27 from Codecademy's Full Stack Engineer Career Path
+### By Anton Vlasenko (AntonV0)  
+## Project Outline
+Number Guesser is a game where the user and the computer choose a number between 1 and 9. The winner is the player whose number is closest to the target number, which is randomised every round. In this project I created 4 functions to make the game work as intended.
+## What I Learned
+  - The logic behind updating the game's round number and score
+  - Comparing two numbers to determine which is closest to the target number
+  - Using the Math.abs function to return absolute number values
+***
+*Please note that in each project, the first commit is always the starter code provided by Codecademy.*
--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+// My version:
+const compareGuesses = (humanScore, computerScore, generateTarget) => {
+  if (
+    Math.abs(generateTarget - humanScore) <=
+    Math.abs(generateTarget - computerScore)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Alternate version:
+/*const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess);
+  const computerDifference = Math.abs(targetGuess - computerGuess);
+  return humanDifference <= computerDifference;
+};*/
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else if (winner === "computer") {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
